5fa788860d1b20ff1e7ef140db0ad24a3ab9a47a
Aditya Agarwal <adityaagarwal0081@gmail.com>
2025-05-30 03:11:43 +0530

    practice

--- 01-Basics/Main.java
+++ 01-Basics/Main.java
@@ -1,5 +1,7 @@
 public class Main {
     public static void main(String[] args) {
+        System.out.print("Hello, Java from VS Code!\n");
+        System.out.println("Hello, Java from VS Code!");
         System.out.println("Hello, Java from VS Code!");
     }
 }
--- 01-Basics/boilerplate.java
+++ 01-Basics/boilerplate.java
@@ -1,5 +1,5 @@
 public class boilerplate {
     public static void main (String[] args){
-    
+   
     }
 }
