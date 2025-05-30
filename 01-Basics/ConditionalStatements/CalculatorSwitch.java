b531da30245f454dc3544d67b9f0c1ae9d8c82d8
Aditya Agarwal <adityaagarwal0081@gmail.com>
2025-05-31 04:10:13 +0530

    commit

---
+++ 01-Basics/ConditionalStatements/CalculatorSwitch.java
@@ -0,0 +1,47 @@
+package ConditionalStatements;
+import java.util.*;
+
+public class CalculatorSwitch {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter two numbers: ");
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+
+          sc.nextLine();
+          
+        System.out.println("Enter operator (+, -, *, /, %): ");
+        String operator = sc.nextLine(); 
+
+        switch(operator){
+            case "+":
+                System.out.println("Result: " + (a + b));
+                break;
+            case "-":
+                System.out.println("Result: " + (a - b));
+                break;
+            case "*":
+                System.out.println("Result: " + (a * b));
+                break;
+            case "/":
+                if (b != 0) {
+                    System.out.println("Result: " + (a / b));
+                } else {
+                    System.out.println("Cannot divide by zero!");
+                }
+                break;
+            case "%":
+                if (b != 0) {
+                    System.out.println("Result: " + (a % b));
+                } else {
+                    System.out.println("Cannot modulo by zero!");
+                }
+                break;
+            default:
+                System.out.println("Invalid operator!");
+        }
+
+        sc.close();
+    }
+}
