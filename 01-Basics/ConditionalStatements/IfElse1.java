cf38c7d42793e5f6bed05b6f9ca26b3016730c06
Aditya Agarwal <adityaagarwal0081@gmail.com>
2025-05-31 01:46:42 +0530

    ifelse prac

---
+++ 01-Basics/ConditionalStatements/IfElse1.java
@@ -0,0 +1,20 @@
+ 
+
+import java.util.*;
+
+public class IfElse1{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("enter your age: ");
+        int age = sc.nextInt();
+
+        if(age >= 18){
+            System.out.println("let him drive and vote");
+        } else {
+            System.out.println("stay inside boi");
+        }
+
+        sc.close();
+    }
+}
