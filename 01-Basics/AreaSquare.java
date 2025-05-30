1fbe8edd7dceec574cc0f419005147edc0d09424
Aditya Agarwal <adityaagarwal0081@gmail.com>
2025-05-30 16:55:59 +0530

    practice sheet

---
+++ 01-Basics/AreaSquare.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class AreaSquare {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter the side length of the square: ");
+        double side = sc.nextDouble();
+
+        double area = side * side;
+
+        System.out.println("Area of the square: " + area);
+
+        sc.close();
+    }
+}
