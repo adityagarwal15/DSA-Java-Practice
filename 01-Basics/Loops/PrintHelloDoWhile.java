9813cd834fff2101a861069671c39d28055f8dc4
Aditya Agarwal <adityaagarwal0081@gmail.com>
2025-05-31 17:21:40 +0530

    dowhile prac

---
+++ 01-Basics/Loops/PrintHelloDoWhile.java
@@ -0,0 +1,12 @@
+package Loops;
+
+public class PrintHelloDoWhile {
+    public static void main(String[] args){
+        int counter  = 1;
+        do{
+            System.out.println("hello");
+            counter++;
+        }
+        while(counter<=10);
+    }
+}
