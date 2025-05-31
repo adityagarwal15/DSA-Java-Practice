068e976b7c7b25a17594de874d7619af5f04cf4a
Aditya Agarwal <adityaagarwal0081@gmail.com>
2025-05-31 17:05:14 +0530

    three methods of reversing a number

--- 01-Basics/Loops/GoodQuestions/ReverseNumber.java
+++ 01-Basics/Loops/GoodQuestions/ReverseNumber.java
@@ -9,16 +9,32 @@ public class ReverseNumber {
         System.out.println("Enter the number: ");
         int n = sc.nextInt();
 
+        //method 1
+
         // System.out.print("Reversed digits: ");
         // for (; n > 0; n = n / 10) {
         //     System.out.print(n % 10);
         // }
 
-         System.out.print("Reversed number: ");
-        while (n > 0) {
-            System.out.print(n % 10); // Print last digit
-            n = n / 10;               // Remove last digit
-        }
+        //method 2
+
+        //  System.out.print("Reversed number: ");
+        // while (n > 0) {
+        //     System.out.print(n % 10); // Print last digit
+        //     n = n / 10;               // Remove last digit
+        // }
+
+        //method 3
+
+        int rev = 0; // Initialization (important!)
+            while (n > 0) {
+            int lastDigit = n % 10;
+            rev = rev * 10 + lastDigit;
+            n = n / 10;
+}
+
+        System.out.println("Reversed number: " + rev);
+
 
         sc.close();
     }
