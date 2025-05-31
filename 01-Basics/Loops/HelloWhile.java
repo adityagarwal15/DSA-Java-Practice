1942547f2862cab58a4fffa69b47b056648e1937
Aditya Agarwal <adityaagarwal0081@gmail.com>
2025-05-31 05:33:29 +0530

    commit while prac

---
+++ 01-Basics/Loops/HelloWhile.java
@@ -0,0 +1,12 @@
+package Loops;
+
+public class HelloWhile {
+    public static void main(String[] args){
+    int counter = 0;
+
+   while(counter<5){
+    System.out.println("Hello");
+    counter++;
+   }
+}
+}
---
+++ 01-Basics/Loops/PrintOneToTen.java
@@ -0,0 +1,12 @@
+package Loops;
+
+public class PrintOneToTen {
+    public static void main(String[] args){
+//using while loop printing numbers 1 to 10
+        int counter = 1;
+        while(counter<11){
+            System.out.println((counter));
+            counter++;
+        }
+    }
+}
