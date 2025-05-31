ac1fa2538e7f359c135c138bc9137613068773c4
Aditya Agarwal <adityaagarwal0081@gmail.com>
2025-05-31 05:48:45 +0530

    commit

--- 01-Basics/Loops/HelloWhile.java
+++ 01-Basics/Loops/HelloWhile.java
@@ -2,11 +2,17 @@ package Loops;
 
 public class HelloWhile {
     public static void main(String[] args){
-    int counter = 0;
+//     int counter = 0;
 
-   while(counter<5){
-    System.out.println("Hello");
-    counter++;
-   }
+//    while(counter<5){
+//     System.out.println("Hello");
+//     counter++;
+//    }
+
+
+//printing numbers 1 to 10 using for loop
+for(int i = 1; i<=10; i++){
+    System.out.println(i);
+}
 }
 }
