15821ac2b230f2a05ea11f7b23976eebc09a7a01
Aditya Agarwal <adityaagarwal0081@gmail.com>
2025-06-02 02:57:22 +0530

    linear search

--- 02-Arrays/LinearSearch.java
+++ 02-Arrays/LinearSearch.java
@@ -3,6 +3,15 @@ import java.util.*;
 
 public class LinearSearch {
 
+    public static int MenuSearch(String menu[], String menu_key){
+        for(int i =0; i<menu.length;i++){
+            if(menu[i] == menu_key){
+                return i;
+            }
+        }
+        return -1;
+    }
+
     public static int Linear(int numbers[], int key){
         
         for (int i = 0; i<numbers.length; i++){
@@ -15,11 +24,28 @@ public class LinearSearch {
 
     public static void main(String[] args){
         int numbers[] = {1,2,3,4,5,6,7,8,9,10};
-        int key = 7;
+        int key = 17;
+
+        String menu[] = {"dosa", "idli", "golgappe"};
+        String menu_key = "idli";
 
         int result = Linear(numbers,key);
-        System.out.print("key found at index: " + result);
+        if(result == -1){
+            System.out.print("key not found");
+        }
+        else{
+        System.out.println("key found at index: " + result);
+        }
 
+        System.out.println();
+
+        int menu_result = MenuSearch(menu,menu_key);
+        if(menu_result == -1){
+            System.out.print("key not found");
+        }
+        else{
+        System.out.println("menu key found at index: " + menu_result);
+        }
 
     }
 }
