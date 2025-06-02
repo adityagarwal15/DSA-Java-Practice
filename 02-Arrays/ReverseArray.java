63f5b774d93862887f4b754b4c5b461a60ecbe86
Aditya Agarwal <adityaagarwal0081@gmail.com>
2025-06-02 05:32:45 +0530

    reverse array imp

--- 02-Arrays/ReverseArray.java
+++ 02-Arrays/ReverseArray.java
@@ -1,3 +1,27 @@
 public class ReverseArray {
-    
+
+    public static void Reverse(int numbers[]) {
+        int start = 0;
+        int end = numbers.length - 1;
+
+        while(start < end) {
+            int temp = numbers[end];
+            numbers[end] = numbers[start];
+            numbers[start] = temp;
+
+            start++;
+            end--;
+        }
+    }
+
+    public static void main(String[] args) {
+        int numbers[] = {2, 4, 6, 8, 10};
+
+        Reverse(numbers);
+
+        System.out.print("Reversed array is: ");
+        for(int i = 0; i < numbers.length; i++) {
+            System.out.print(numbers[i] + " ");
+        }
+    }
 }
