efd408d0436ec48521b5415bde3f44f6966f77ca
Aditya Agarwal <adityaagarwal0081@gmail.com>
2025-06-02 19:20:26 +0530

    print sum, max, min of each subarrays

--- 02-Arrays/PrintSumMaxMinSubArray.java
+++ 02-Arrays/PrintSumMaxMinSubArray.java
@@ -1,4 +1,4 @@
-public class PrintSubArrays {
+public class PrintSumMaxMinSubArray {
 
     public static void SubArr(int numbers[]) {
         int count = 0; // to count the total number of subarrays
@@ -8,24 +8,35 @@ public class PrintSubArrays {
         //     int start = i; // start point
         //     for (int j = i; j < numbers.length; j++) {
         //         int end = j; // end point
+        //         int sum = 0, max = Integer.MIN_VALUE, min = Integer.MAX_VALUE;
         //         for (int k = start; k <= end; k++) { // loop from start to end
         //             System.out.print(numbers[k] + " ");
+        //             sum += numbers[k];
+        //             max = Math.max(max, numbers[k]);
+        //             min = Math.min(min, numbers[k]);
         //         }
+        //         System.out.println(" | Sum: " + sum + ", Max: " + max + ", Min: " + min);
         //         count++;
-        //         System.out.println();
         //     }
         //     System.out.println();
         // }
 
         // Version without using start and end
-        for (int i = 0; i < numbers.length; i++) {   //start
+        for (int i = 0; i < numbers.length; i++) { // start
             for (int j = i; j < numbers.length; j++) {
+                int sum = 0;
+                int max = Integer.MIN_VALUE;   // - infinity
+                int min = Integer.MAX_VALUE;   // + infinity
+
                 for (int k = i; k <= j; k++) {
-                    System.out.print(numbers[k] + " ");
+                    System.out.print(numbers[k] + " ");  
+                    sum += numbers[k];   // sum = sum + numbers[k]  //this will keep running until we get the sum, outermost loop wait for the innermost loops
+                    max = Math.max(max, numbers[k]);
+                    min = Math.min(min, numbers[k]);
                 }
 
+                System.out.println("| Sum: " + sum + ", Max: " + max + ", Min: " + min);
                 count++; // increment count once 1 subarray is printed
-                System.out.println();
             }
             System.out.println();
         }
@@ -38,3 +49,5 @@ public class PrintSubArrays {
         SubArr(numbers);
     }
 }
+
+
