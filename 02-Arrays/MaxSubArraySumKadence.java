e3b299da17084704c88deeabe4fe45ce67060e32
Aditya Agarwal <adityaagarwal0081@gmail.com>
2025-06-03 04:01:06 +0530

    kadence algo convering the case when array has all negative

--- 02-Arrays/MaxSubArraySumKadence.java
+++ 02-Arrays/MaxSubArraySumKadence.java
@@ -22,3 +22,27 @@ public class MaxSubArraySumKadence {
         Kadence(numbers);
     }
 }
+
+
+// for the case when in the array all numbers are negative 
+
+// public class MaxSubArraySumKadence {
+
+//     public static void Kadence(int numbers[]) {
+//         int currSum = numbers[0];
+//         int maxSum = numbers[0];
+
+//         for (int i = 1; i < numbers.length; i++) {
+//             currSum = Math.max(numbers[i], currSum + numbers[i]);  // extend or start fresh
+//             maxSum = Math.max(maxSum, currSum);                    // track max
+//         }
+
+//         System.out.println("Max subarray sum is: " + maxSum);
+//     }
+
+//     public static void main(String[] args) {
+//         int numbers[] = {-2, -3, -1, -4};
+//         Kadence(numbers);
+//     }
+// }
+
