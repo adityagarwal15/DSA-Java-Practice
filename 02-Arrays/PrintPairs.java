f4e4201ee458c276dcf6f3779f3812ec314b48b8
Aditya Agarwal <adityaagarwal0081@gmail.com>
2025-06-02 17:25:29 +0530

    print pairs in an array

--- 02-Arrays/PrintPairs.java
+++ 02-Arrays/PrintPairs.java
@@ -4,13 +4,18 @@ public class PrintPairs {
 
     // Method to print all pairs from the array
     public static void Print(int numbers[]) {
+        int totalPairs = 0; // to find total pairs
+
         for (int i = 0; i < numbers.length; i++) {
             int current = numbers[i];
             for (int j = i + 1; j < numbers.length; j++) {
                 int secondPair = numbers[j];
-                System.out.println("(" + current + ", " + secondPair + ")");
+                System.out.print("(" + current + ", " + secondPair + ")");
+                totalPairs++;
             }
+            System.out.println();
         }
+        System.out.print("total pairs: " + totalPairs);
     }
 
     // we are printing everything in the method itself because this is eassier
