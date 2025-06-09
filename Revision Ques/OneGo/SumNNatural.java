8fe43eaf3191539d1ef17f2b0624e40ff52a8645
Aditya Agarwal <adityaagarwal0081@gmail.com>
2025-06-10 05:15:47 +0530

    prac

--- Revision Ques/OneGo/SumNNatural.java
+++ Revision Ques/OneGo/SumNNatural.java
@@ -2,7 +2,21 @@ package OneGo;
 import java.util.*;
 
 public class SumNNatural {
-    public static void main(String[] args){
 
+    public static void Sum(int n){
+        int sum=0;
+        for(int i =1; i<=n; i++){
+            sum += i;
+        }
+        System.out.print("sum is: " + sum);
+    }
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("enter how many n natural numbers sum needed: ");
+        int n = sc.nextInt();
+
+        Sum(n);
+        sc.close();
     }
 }
