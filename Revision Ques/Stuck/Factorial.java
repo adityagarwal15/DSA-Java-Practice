b6a5c4eb83a836f6c374ccf64a0b3034dd8df804
Aditya Agarwal <adityaagarwal0081@gmail.com>
2025-06-10 06:26:54 +0530

    prac

---
+++ Revision Ques/Stuck/Factorial.java
@@ -0,0 +1,24 @@
+package Stuck;
+import java.util.*;
+
+public class Factorial {
+
+    public static int getFactorial(int num){
+        int fact = 1;
+        for(int i=1;i<=num;i++){
+            fact *= i;
+        }
+        return fact;
+    }
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("enter number: ");
+        int num = sc.nextInt();
+
+        int result = getFactorial(num);
+        System.out.print("factorial of a number is: " + result);
+
+        sc.close();
+    }
+}
---
+++ Revision Ques/Stuck/MultiplicationTable.java
@@ -0,0 +1,22 @@
+package Stuck;
+import java.util.*;
+
+public class MultiplicationTable {
+
+    public static void table(int n){
+        for(int i=1;i<=10;i++){
+            int result = n * i;
+            System.out.println(result + " "); //keep this inside loop 
+        }
+    }
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("enter number to get table: ");
+        int n = sc.nextInt();
+
+       table(n);
+
+        sc.close();
+    }
+}
