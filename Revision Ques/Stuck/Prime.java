a434cc8dfe6b3ec3ab6e8dbff2bfdcc556bda89d
Aditya Agarwal <adityaagarwal0081@gmail.com>
2025-06-10 05:49:36 +0530

    prime

---
+++ Revision Ques/Stuck/Prime.java
@@ -0,0 +1,33 @@
+package Stuck;
+import java.util.*;
+
+public class Prime {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter the number: ");
+        int n = sc.nextInt();
+
+        boolean isPrime = true;
+
+        if (n <= 1) {
+            isPrime = false;
+        } else {
+            // Loop from 2 to √n (inclusive)
+            for (int i = 2; i <= Math.sqrt(n); i++) {
+                if (n % i == 0) {
+                    isPrime = false;
+                    break;
+                }
+            }
+        }
+
+        if (isPrime) {
+            System.out.println(n + " is a prime number");
+        } else {
+            System.out.println(n + " is not a prime number");
+        }
+
+        sc.close();
+    }
+}
