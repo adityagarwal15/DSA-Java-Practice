243ffca7691802288d15d098934af4cdaaf6471b
Aditya Agarwal <adityaagarwal0081@gmail.com>
2025-06-19 03:41:23 +0530

    remove duplicates

--- 06-Recursions/RemoveDupStrings.java
+++ 06-Recursions/RemoveDupStrings.java
@@ -2,27 +2,29 @@ import java.util.*;
 
 public class RemoveDupStrings {
 
-    public static String removeDuplicates(String str, int index, StringBuilder result, boolean[] map) {
+    public static String removeDuplicates(String str, int index, StringBuilder result) {
         if (index == str.length()) {
             return result.toString();
         }
 
         char currChar = str.charAt(index);
-        if (!map[currChar - 'a']) {
-            map[currChar - 'a'] = true;
+
+        // Check if the character is already in result
+        if (result.indexOf(String.valueOf(currChar)) == -1) {
             result.append(currChar);
         }
 
-        return removeDuplicates(str, index + 1, result, map);
+        return removeDuplicates(str, index + 1, result);
     }
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+
         System.out.print("Enter string to remove duplicates: ");
         String str = sc.nextLine();
 
-        boolean[] map = new boolean[26];  // for lowercase a-z
-        String result = removeDuplicates(str, 0, new StringBuilder(), map);
+        String result = removeDuplicates(str, 0, new StringBuilder());
+
         System.out.println("After removing duplicates: " + result);
 
         sc.close();
