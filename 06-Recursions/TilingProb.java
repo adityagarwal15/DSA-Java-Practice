fbca6a8aef81bb517b5034252a189941671598f7
Aditya Agarwal <adityaagarwal0081@gmail.com>
2025-06-23 02:44:40 +0530

    tiling prob amazon

---
+++ 06-Recursions/TilingProb.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+public class TilingProb {
+
+    //amazon pyq
+     
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("enter number: ");
+        int num = sc.nextInt();
+
+    }
+}
