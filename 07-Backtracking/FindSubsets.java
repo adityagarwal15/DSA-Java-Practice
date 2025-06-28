7890f960de5f570ed2d0572b2662a8b36cd80584
Aditya Agarwal <adityaagarwal0081@gmail.com>
2025-06-29 03:44:24 +0530

    find subsets

---
+++ 07-Backtracking/FindSubsets.java
@@ -0,0 +1,28 @@
+import java.util.*;
+
+public class FindSubsets {
+
+    public static void sets(String str, String ans, int i) {
+        // base case
+        if (i == str.length()) {
+            System.out.println(ans);  // Print subset followed by newline
+            return;
+        }
+
+        // recursion (choices)
+        // Include current character(basically says yes add the current char)
+        sets(str, ans + str.charAt(i), i + 1);  //includes the character
+
+        // Exclude current character(says no, do not add the current char)
+        sets(str, ans, i + 1);    //skips the character
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter string: ");
+        String str = sc.nextLine();
+
+        System.out.println("Subsets:");
+        sets(str, "", 0);
+    }
+}
