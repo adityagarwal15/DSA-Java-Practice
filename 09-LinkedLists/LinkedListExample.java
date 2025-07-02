6f009c6816370cf7dbf0098e0b9e2a9b3bfc8c0c
Aditya Agarwal <adityaagarwal0081@gmail.com>
2025-07-03 00:03:02 +0530

    Linked List

---
+++ 09-LinkedLists/LinkedListExample.java
@@ -0,0 +1,62 @@
+// Node class
+class Node {
+    int data;
+    Node next;
+
+    Node(int data) {
+        this.data = data;
+        this.next = null;
+    }
+}
+
+// LinkedList class
+class LinkedList {
+    Node head;
+    Node tail;
+
+    // Add a new node at the beginning
+    public void addFirst(int data) {
+        Node newNode = new Node(data);
+        newNode.next = head;
+        head = newNode;
+
+        if (tail == null) {
+            tail = newNode;
+        }
+    }
+
+    // Add a new node at the end
+    public void addLast(int data) {
+        Node newNode = new Node(data);
+        if (head == null) {
+            head = newNode;
+            tail = newNode;
+            return;
+        }
+
+        tail.next = newNode;
+        tail = newNode;
+    }
+
+    // Print all elements in the Linked List
+    public void printList() {
+        Node temp = head;
+        while (temp != null) {
+            System.out.print(temp.data + " -> ");
+            temp = temp.next;
+        }
+        System.out.println("null");
+    }
+}
+
+// Main class (must match filename)
+public class LinkedListExample {
+    public static void main(String[] args) {
+        LinkedList list = new LinkedList();
+        list.addFirst(10);
+        list.addFirst(5);
+        list.addLast(20);
+        list.addLast(25);
+        list.printList(); // Expected: 5 -> 10 -> 20 -> 25 -> null
+    }
+}
